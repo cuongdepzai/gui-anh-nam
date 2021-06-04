53a8d6d6780a7b9d8996bd46a9c3d9f5454a8cc1
cuongdepzai <85315293+cuongdepzai@users.noreply.github.com>
2021-06-04 10:47:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,4 +13,19 @@ const CONFIG = {
     messDesc: 'Chuýt sì',
     btnAccept: 'Biếnnn',
     messLink: 'https://hentaivn.tv/'
+    introTitle: 'Chào long Lồn',
+    introDesc: `Con là Cường đây :> `,
+    btnIntro: 'Ấn vào đây để típ nào bae ',
+    title: 'Chọn 1 trong 2 đi con chó ',
+    desc: 'M không thoát được đâu con zai ',
+    btnYes: 'Cho Cường 100tr',
+    btnNo: 'Được Cường bao ăn 1 năm',
+    question:'Chúc mừng bố đã quay vào ô mất lượt',
+    btnReply: 'Ấn vào đây để típ',
+    reply: 'con cẹc',
+    mess: 'Long Dumm vl',
+    messDesc: 'Chuýt sì',
+    btnAccept: 'Biếnnn',
+    messLink: 'https://hentaivn.tv/'
 }
+
