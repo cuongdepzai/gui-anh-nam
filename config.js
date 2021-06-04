176a66fac97e29a5b2ab071fafecac0605d0ad64
cuongdepzai <85315293+cuongdepzai@users.noreply.github.com>
2021-06-04 09:09:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,19 @@
 const CONFIG = {
-    introTitle: 'Chào thằng lòn Đức Anh',
-    introDesc: `Tao là Cường đây`,
-    btnIntro: 'Ấn vào đây',
-    title: 'Bây giờ m có 2 lựa chọn ',
-    desc: 'Nghĩ kĩ nhé ',
-    btnYes: 'Cho tao 2 đôi giầy',
-    btnNo: 'Được tao bao đi ăn lẩu ',
-    question:'Chúc mừng m đã quay vào ô may mắn :))) ',
-    btnReply: 'M lừa tao',
-    reply: 'Đúng rồi đó ',
-    mess: 'Chuýt sì',
-    messDesc: 'Lêu lêu',
-    btnAccept: 'Chào thằng lòn nha :>> ',
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là yêu anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Mãi mãi iu em <3333.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
