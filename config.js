796dcb7ad1335a9194542fec4f08366790a3bcca
cuongdepzai <85315293+cuongdepzai@users.noreply.github.com>
2021-06-04 12:40:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,31 +1,17 @@
 const CONFIG = {
-    introTitle: 'Chào long Lồn',
-    introDesc: `Con là Cường đây :> `,
+    introTitle: 'Chào Cả nhà iu',
+    introDesc: `Mình là Cường đây :> `,
     btnIntro: 'Ấn vào đây để típ nào bae ',
     title: 'Chọn 1 trong 2 đi con chó ',
     desc: 'M không thoát được đâu con zai ',
     btnYes: 'Cho Cường 100tr',
     btnNo: 'Được Cường bao ăn 1 năm',
-    question:'Chúc mừng bố đã quay vào ô mất lượt',
+    question:'Chúc mừng cậu đã quay vào ô mất lượt',
     btnReply: 'Ấn vào đây để típ',
     reply: 'con cẹc',
-    mess: 'Long Dumm vl',
+    mess: 'Xong rồi ',
     messDesc: 'Chuýt sì',
     btnAccept: 'Biếnnn',
-    messLink: 'https://hentaivn.tv/'
-    introTitle: 'Chào long Lồn',
-    introDesc: `Con là Cường đây :> `,
-    btnIntro: 'Ấn vào đây để típ nào bae ',
-    title: 'Chọn 1 trong 2 đi con chó ',
-    desc: 'M không thoát được đâu con zai ',
-    btnYes: 'Cho Cường 100tr',
-    btnNo: 'Được Cường bao ăn 1 năm',
-    question:'Chúc mừng bố đã quay vào ô mất lượt',
-    btnReply: 'Ấn vào đây để típ',
-    reply: 'con cẹc',
-    mess: 'Long Dumm vl',
-    messDesc: 'Chuýt sì',
-    btnAccept: 'Biếnnn',
-    messLink: 'https://hentaivn.tv/'
+    messLink: 'https://www.facebook.com/vitkeumeomeoz'
 }
-
+    
